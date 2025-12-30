f8fd62a4a992d8656491ea6190c64846eb96969d
Arihant Pratap Singh <arihantiit7@gmail.com>
2025-12-30 23:24:25 +0530

    Time: 1 ms (91.06%), Space: 8.9 MB (14.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,17 +1,45 @@
+#include <stdio.h>
 #include <stdlib.h>
+
+struct numsIndex{
+    int element;
+    int index;
+};
+
+int comp(const void *a, const void *b) {
+    return (((struct numsIndex*)a)->element - ((struct numsIndex*)b)->element);
+}
+
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int* result = (int*)malloc(2 * sizeof(int));
     *returnSize = 2;
-
+    struct numsIndex numbers[numsSize];
+    //sorting
     for (int i = 0; i < numsSize; i++) {
-        for (int j = i + 1; j < numsSize; j++) {
-            if (nums[i] + nums[j] == target) {
-                result[0] = i;
-                result[1] = j;
-                return result;
-            }
+        numbers[i].element = nums[i];
+        numbers[i].index=i;
+    }
+
+    qsort(numbers, numsSize, sizeof(struct numsIndex), comp);
+    //Two pointer
+    int i = 0;
+    int sum;
+    int j = numsSize-1;
+    while (i<j) {
+        sum = numbers[i].element+numbers[j].element;
+        if (sum == target) {
+            result[0]=numbers[i].index;
+            result[1]=numbers[j].index;
+            return result;
+        }
+        else if (sum<target) {
+            i++;
+        }
+        else {
+            j--;
         }
     }
+    *returnSize=0;
     return NULL;
 }
