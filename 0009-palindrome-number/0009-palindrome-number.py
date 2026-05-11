38c8993782f19cc31a3cc13b66822dcd8f6aa49e
Arihant Pratap Singh <arihantiit7@gmail.com>
2026-05-11 15:12:57 +0530

    Time: 15 ms (35.33%), Space: 12.4 MB (54.74%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def digit_counter(self,x):
+        n = x
+        count=0
+        while n>0 :
+            count+=1
+            n = n//10
+        
+        
+        return count
+    def extractDigits(self,x):
+        n = x
+        digits=[]
+        while n>0 :
+            digit = n%10
+            digits.append(digit)
+            n = n//10
+        
+        return digits
+    def isPalindrome(self, x):
+        if x<0 :
+            return False
+        if x>=0 and x<=9 :
+            return True
+        n = x
+        length = self.digit_counter(n)
+        rDigits = self.extractDigits(n)
+        sum=0
+        j=length-1
+        for i in range(0,length):
+            sum+= rDigits[i]*(10**j)
+            j-=1
+        
+        return sum==n
+        
